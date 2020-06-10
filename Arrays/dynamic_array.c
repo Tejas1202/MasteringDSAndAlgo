6751a394781905372b7905ea3c749b3f4b60d56e
Tejas P. Gondalia <tejas.gondalia@gmail.com>
2020-06-10 09:57:06 +0530

    First commit

---
+++ Arrays/dynamic_array.c
@@ -0,0 +1,27 @@
+/ Creating Dynamic arrays
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int *p,*q;
+    p = (int *)malloc(3 * sizeof(int));
+    p[0] = 2;
+    p[1] = 4;
+    p[2] = 6;
+    q = (int *)malloc(6 * sizeof(int));
+    
+    for(int i = 0; i < 3; i++)
+    {
+        q[i] = p[i];
+    }
+    
+    free(p);
+    p = q;
+    q = NULL;
+    
+    for(int i = 0; i < 6; i++)
+    {
+        printf("%d ", p[i]);
+    }
+	return 0;
+}
---
+++ Arrays/two_dimensional_arrays.c
@@ -0,0 +1,30 @@
+// Creating 2D arrays with 3 different methods
+#include <stdio.h>
+
+int main(void) {
+    // Whole 2D array created in stack
+	int A[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+	
+	// Array of pointers
+	int *B[3]; // Created in stack
+	B[0] = (int *)malloc(4 * sizeof(int)); // In heap
+	B[1] = (int *)malloc(4 * sizeof(int));
+	B[2] = (int *)malloc(4 * sizeof(int));
+	
+	// Double pointers
+	int **C;
+	C = (int **)malloc(3 * sizeof(int));
+	C[0] = (int *)malloc(4 * sizeof(int));
+	C[1] = (int *)malloc(4 * sizeof(int));
+	C[2] = (int *)malloc(4 * sizeof(int));
+	
+	for(int i = 0; i <  3; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+	return 0;
+}
